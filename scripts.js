4a2b6ccd51368a8ec93130453f8337b490f38950
booban1 <simonnyongesa6@gmail.com>
2020-05-14 01:22:57 +0300

    little js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,16 @@ var item = document.getElementById("item");
 var closeBtn = document.getElementById("closeBtn");
 var itemBody = document.getElementById("itemView");
 
+var itemObj = {
+        itemName: "Luciano Shoel",
+        itemType: "Gucini",
+        itemPrice: "1490"
+    };
+    
+var itemName = document.getElementById("item-name").innerHTML = itemObj.itemName;
+var itemType = document.getElementById("item-type").innerHTML = itemObj.itemType;
+var itemPrice = document.getElementById("item-price").innerHTML = "Ksh. " + itemObj.itemPrice;
+
 item.addEventListener( 'click', () => {
     itemBody.style.display = 'block';
     console.log("i am running");
@@ -12,7 +22,11 @@ closeBtn.addEventListener( 'click', () => {
     console.log("i am running");
 });
 
-// itemBody.addEventListener( 'click', () => {
-//     itemBody.style.display = 'none';
-// });
 
+itemBody.addEventListener( 'click', () => {
+    itemBody.style.display = 'none';
+});
+
+
+// gets sample from here
+// https://www.home24.de/produkt/sessel-garbo-i-webstoff-stoff-saia-hellgrau-eiche-dunkel
